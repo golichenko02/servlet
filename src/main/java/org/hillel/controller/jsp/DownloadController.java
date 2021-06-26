8d75173ef276f0f1fb0083d3e55b0b7a9859ad3e
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2021-06-27 02:55:48 +0300

    HW9 - moved

--- src/main/java/org/hillel/controller/jsp/DownloadController.java
+++ src/main/java/org/hillel/controller/jsp/DownloadController.java
@@ -1,9 +1,7 @@
-package org.hillel.controller;
+package org.hillel.controller.jsp;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
